3ab4354091464aa8e3f3b0b6f1efe09aa00956ff
jonasPereira533 <jonaspereirasilvaa92@gmail.com>
2026-08-29 09:29:00 -0300

    feat header: criei o main-header, componete em vue logo + botão de login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from "./components/main-header.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <main-header />
 </template>
