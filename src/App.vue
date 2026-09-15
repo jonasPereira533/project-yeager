c5fb72b07ccfeed9a7ecaf4a99794d95a4499292
jonasPereira533 <jonaspereirasilvaa92@gmail.com>
2026-09-14 21:53:39 -0300

    removed: old routes

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import HomePage from './views/main-page.vue'
-import CasesPage from './views/case-page.vue'
-
-type View = 'home' | 'cases'
-
-const currentView = ref<View>('home')
-
-function goToCases() {
-  currentView.value = 'cases'
-}
-
-function goHome() {
-  currentView.value = 'home'
-}
-
-
-function onSelectCase(caseId: string) {
-  console.log('Caso selecionado:', caseId)
-}
 </script>
 
 <template>
